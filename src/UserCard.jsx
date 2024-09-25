68d6599a81b6531098627ae82b53bf91335670a8
roshniaxel <roshni.upadhyay@axelerant.com>
2024-09-25 15:09:23 +0530

    User card

---
+++ src/UserCard.jsx
@@ -0,0 +1,15 @@
+export function UserCard({name, age, phone, address}) {
+    return (
+        <div className="card">
+            <h2 className="name">{name}</h2>
+            <div className="body">
+                <div className="label">Age:</div>
+                <div>{age}</div>
+                <div className="label">Phone:</div>
+                <div>{phone}</div>
+                <div className="label">Address:</div>
+                <div>{address}</div>
+            </div>
+        </div>
+    )
+}
